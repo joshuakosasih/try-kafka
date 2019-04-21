2311dc92e4267008497cb0addf1aa122ee828324
Joshua Kosasih <joshua.kosasih@gmail.com>
2019-04-21 17:55:01 +0700

    update naming for consumer

--- new_str_long_consumer.sh
+++ new_str_long_consumer.sh
@@ -4,3 +4,8 @@ kafka_2.12-2.2.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:$por
     --topic $consumer \
     --from-beginning \
     --formatter kafka.tools.DefaultMessageFormatter \
+    --property print.key=true \
+    --property print.value=true \
+    --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
