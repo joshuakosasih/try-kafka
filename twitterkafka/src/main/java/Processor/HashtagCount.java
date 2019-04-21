a36739b35d965cee5c9b987afb0a36f38261868e
Joshua Kosasih <joshua.kosasih@gmail.com>
2019-04-21 22:45:41 +0700

    add hashtag counter stream processor

---
+++ twitterkafka/src/main/java/Processor/HashtagCount.java
@@ -0,0 +1,60 @@
+package Processor;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
+
+public class HashtagCount {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-hashtagcount");
+        // assuming that the Kafka broker this application is talking to runs on local machine with port 9092
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> hashtags = builder.stream("hashtags");
+        KTable<String, Long> counts = hashtags.groupBy((key, value) -> value).count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>> as("counts-store"));
+        counts.toStream().to("hashtag-count", Produced.with(Serdes.String(), Serdes.Long()));
+
+
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+
+    }
+}
